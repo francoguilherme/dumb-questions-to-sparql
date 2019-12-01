f4122bda852888c7522fb6007c81d3caf43a56d6
Guilherme Franco <guilherme.franco@pigpeg.com>
2019-12-01 16:00:22 -0300

    Quepy endpoint added

--- src/App.js
+++ src/App.js
@@ -6,43 +6,27 @@ import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import PersonCard from "./components/PersonCard"
-import PlaceCard from "./components/PlaceCard"
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            entity: Object,
-            entities: [],
-            searchInput: ""
-        }
+            searchInput: "",
+            query: "",
+            result: [],
+            error: ""
+        };
     }
 
     searchInput = '';
 
-    appendEntity = (entity) => {
-        let list = this.state.entities;
-        list.push(entity);
-        list.filter((v,i) => list.indexOf(v) === i);
-        this.setState({entities: list});
-    };
-
     updateInputValue(evt) {
-        this.searchInput = this.camelize(evt.target.value)
-    }
-
-    camelize(str) {
-        return str.replace(/(?=\b(af|von|van|de|of|zu|du|do|dos|da|das|e)\b)|(?:^\w|[A-Z]|\b\w|\s+|)/g, function(match, index) {
-            if (/\s+/.test(match)) return "_";
-
-            return match.toUpperCase();
-        });
+        this.searchInput = evt.target.value
     }
 
     clearData = () => {
-        this.setState({entities:[]})
+        this.setState({query:"", result: [], error: ""})
     };
 
     onKeyPress = (e) => {
@@ -52,82 +36,73 @@ class App extends React.Component {
     };
 
     onFetchData = () => {
-        const genericParams = "&property=dbo%3AbirthDate&property=georss:point&pretty=NONE&limit=1&offset=0&key=1234&oldVersion=false";
-        const personParams = "&property=foaf%3Adepiction&property=foaf%3Aname&property=dct%3Adescription&property=dbp%3Anationality&property=dbo%3AbirthDate&property=dbo%3AbirthPlace&property=dbo%3AdeathDate&property=dbo%3AdeathPlace&property=dbo%3AalmaMater&property=dbo%3Aresidence&property=dbo%3Aspouse&pretty=NONE&limit=1&offset=0&key=1234&oldVersion=false";
-        const placeParams = "&property=foaf%3Adepiction&property=foaf%3Aname&property=foaf:nick&property=dbo:populationTotal&property=dbo:city&property=dbo:country&property=dbo:capital&property=georss:point&property=dbo:type&property=dbp:established&pretty=NONE&limit=1&offset=0&key=1234&oldVersion=false";
-
         this.setState({searchInput: this.searchInput}, () => {
-            let searchParams = '';
             axios
-                .get("https://recinfo-dbpedia-api.herokuapp.com/api/1.0.0/values?entities=".concat(this.state.searchInput, genericParams),
-                    {headers: {"Accept":"application/json"}})
+                .post("https://quepy-api.herokuapp.com/", {"question":this.state.searchInput})
                 .then(response => {
-                    const data = response.data.results.bindings[0];
-                    if (data["dbobirthDate"] != null) {
-                        searchParams = personParams
-                    } else if (data["georsspoint"] != null) {
-                        searchParams = placeParams
-                    }
-
-                    axios
-                        .get("https://recinfo-dbpedia-api.herokuapp.com/api/1.0.0/values?entities=".concat(this.state.searchInput, searchParams),
-                            {headers: {"Accept":"application/json"}})
-                        .then(response => {
-                            const data = response.data.results.bindings[0];
-                            let entity = {};
-                            for (let param in data) {
-                                entity[param] = data[param].value;
-                            }
-                            this.appendEntity(entity);
-                        })
-                        .catch(error => console.log(error));
+                    console.log(response.data);
+                    this.setState(response.data);
                 })
-                .catch(error => console.log(error));
         });
     };
 
     render() {
         return (
-            <div className="mt-5 text-center">
+            <div className="mt-5 text-center mb-5">
                 <h6 style={{fontSize:"60px"}} className="text-white mb-5">
-                    DBpedia Searcher
+                    Ask something
                 </h6>
                 <Paper style={{
                     padding: '2px 10px',
                     display: 'flex',
                     alignItems: 'center',
-                    width: 'auto',
+                    width: '400px',
                     position: 'absolute',
                     left: '50%',
                     transform: 'translate(-50%, 0)'
                 }}>
                     <InputBase
-                        placeholder="Search for someone"
+                        fullWidth
+                        placeholder="What is the cast of Friends?"
                         onKeyPress={this.onKeyPress}
                         type="text" onChange={evt => this.updateInputValue(evt)}
                     />
-                    <IconButton type="button" onClick={this.onFetchData} aria-label="Search" style={{outline:0}}>
-                        <i className="fas fa-search"/>
-                    </IconButton>
-                    <Divider style={{width: 2, height: 28, margin: 4,}}/>
-                    <Tooltip title="Clear searches">
-                        <IconButton type="button" onClick={this.clearData} aria-label="Search" style={{outline:0}}>
-                            <i className="fas fa-eraser"/>
+                    <div style={{display: 'flex', position:"relative"}}>
+                        <IconButton type="button" onClick={this.onFetchData} aria-label="Search" style={{outline:0}}>
+                            <i className="fas fa-search"/>
                         </IconButton>
-                    </Tooltip>
+                        <Divider style={{width: 2, height: 28, margin: 4}}/>
+                        <Tooltip title="Clear searches">
+                            <IconButton type="button" onClick={this.clearData} aria-label="Search" style={{outline:0}}>
+                                <i className="fas fa-eraser"/>
+                            </IconButton>
+                        </Tooltip>
+                    </div>
                 </Paper>
                 <button type="button" onClick={this.clearData} className="ml-5">Clear searches</button>
 
-                <div className="row">
-                    {this.state.entities.map(entity => {
-                        if (entity.dbobirthDate != null){
-                            return <div key={entity.foafname} className="col"><PersonCard appendEntity={this.appendEntity} {...entity}/></div>
-                        } else if (entity.georsspoint != null) {
-                            return <div key={entity.foafname} className="col"><PlaceCard appendEntity={this.appendEntity} {...entity}/></div>
-                        } else {
-                            return <i/>
-                        }
-                    })}
+                <div className="mt-5">
+                    {(this.state.error === "" && this.state.query !== "") && <div className="mx-auto">
+                        <div style={{maxWidth:"500px", margin:"auto"}} className="text-white">
+                            <h3>Answer:</h3>
+                            {this.state.result.map(result => {
+                                return <p>- {result}</p>
+                            })}
+                        </div>
+                        <Divider light style={{width:"100%", height: 4}}/>
+                        <div className="ml-5 mt-3 text-left text-white">
+                            <h5>SPARQL Query:</h5>
+                            <div style={{display:"block", whiteSpace:"pre-wrap"}}>
+                                {this.state.query}
+                            </div>
+                        </div>
+                    </div>}
+
+                    {(this.state.error !== "") && <div className="text-center">
+                        <div>
+                            <div className="text-danger">Error:<p>{this.state.error}</p></div>
+                        </div>
+                    </div>}
                 </div>
             </div>
         );
